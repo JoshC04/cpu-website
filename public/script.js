419d6bbfec649b5358f7f8fb9e589782cb4c3b19
Joshua Cook <jc157@email.sc.edu>
2023-11-05 02:38:15 -0500

    editing assignment 13

---
+++ public/script.js
@@ -0,0 +1,54 @@
+const displayCPU = async () => {
+  let response = await fetch("http://localhost:3000/api/cpus");
+  let cpuJSON = await response.json();
+  let cpuContainer = document.getElementById("container");
+
+  cpuJSON.forEach((cpus) => {
+    let section = document.createElement("section");
+    section.classList.add("cpu-style");
+    cpuContainer.appendChild(section);
+
+    let h3 = document.createElement("h3");
+    h3.classList.add("typography");
+    h3.innerText = cpus.name;
+    section.appendChild(h3);
+
+    let img = document.createElement("img");
+    img.src = "http://localhost:3000/" + cpus.img;
+    section.appendChild(img);
+
+    let speed = document.createElement("p");
+    speed.classList.add("typography");
+    speed.innerText = `Speed: ${cpus.speed} GHz`;
+    section.appendChild(speed);
+
+    let cores = document.createElement("p");
+    cores.classList.add("typography");
+    cores.innerText = `Cores: ${cpus.cores}`;
+    section.appendChild(cores);
+
+    let threads = document.createElement("p");
+    threads.classList.add("typography");
+    threads.innerText = `Threads: ${cpus.threads}`;
+    section.appendChild(threads);
+
+    let p = document.createElement("p");
+    p.classList.add("typography");
+    p.innerText = "Compatible Motherboards:";
+    section.appendChild(p);
+
+    let ul = document.createElement("ul");
+    section.appendChild(ul);
+
+    cpus.motherboards.forEach((motherboards) => {
+      let li = document.createElement("li");
+      li.classList.add("typography", "cpu-list");
+      li.innerText = motherboards;
+      ul.appendChild(li);
+    });
+  });
+};
+
+window.onload = () => {
+  displayCPU();
+};
--- server.js
+++ server.js
@@ -1,8 +1,71 @@
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/cpus", (req, res) => {
+  const cpus = [];
+  cpus[0] = {
+    name: "Intel Core i5-13400",
+    speed: 4.6,
+    cores: 6,
+    threads: 16,
+    motherboards: ["H610M-HDV", "B660M Pro RS", "PRIME H610M-E D4-CSM"],
+    img: "images/IntelCorei5-13400.jpg",
+  };
+  cpus[1] = {
+    name: "AMD Ryzen 7 7800X3D",
+    speed: 5,
+    cores: 8,
+    threads: 16,
+    motherboards: [
+      "Gigabyte B650 AORUS ELITE AX",
+      "ASRock B650M Pro RS WiFi",
+      "Asus ROG STRIX B650-A GAMING WIFI",
+    ],
+    img: "images/AMDRyzen77800X3D.jpg",
+  };
+  cpus[2] = {
+    name: "AMD Ryzen 9 7950X3D",
+    speed: 4.2,
+    cores: 16,
+    threads: 32,
+    motherboards: [
+      "GIGABYTE X670E AORUS Master",
+      "NZXT N7 B650E Gaming Wi-Fi 6",
+      "ASUS ROG Crosshair X670E Extreme",
+    ],
+    img: "images/AMDRyzen97950X3D.jpg",
+  };
+  cpus[3] = {
+    name: "Intel Core i5-13600K",
+    speed: 3.5,
+    cores: 14,
+    threads: 20,
+    motherboards: [
+      "Gigabyte Z790 AORUS ELITE AX",
+      "MSI MAG Z790 TOMAHAWK WIFI",
+      "MSI PRO B760-P WIFI DDR4",
+    ],
+    img: "images/IntelCorei5-13600K.jpg",
+  };
+  cpus[4] = {
+    name: "Intel Core i3-12100F",
+    speed: 2.5,
+    cores: 4,
+    threads: 8,
+    motherboards: [
+      "Gigabyte Z790 AORUS ELITE AX",
+      "MSI MAG Z790 TOMAHAWK WIFI",
+      "ASRock B660M Pro RS",
+    ],
+    img: "images/IntelCorei3-12100F.jpg",
+  };
+
+  res.json(cpus);
 });
 
 app.listen(3000, () => {
